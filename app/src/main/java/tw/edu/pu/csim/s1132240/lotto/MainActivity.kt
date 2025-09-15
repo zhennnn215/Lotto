4851a53471e9365c5f0d7514bd6c8c83db148e8b
s1132240 <doreen950215@gmail.com>
2025-09-15 16:46:37 +0800

    第三次commit (加入按鈕)

--- app/src/main/java/tw/edu/pu/csim/s1132240/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1132240/lotto/MainActivity.kt
@@ -1,11 +1,14 @@
 package tw.edu.pu.csim.s1132240.lotto
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -31,9 +34,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Play(modifier: Modifier = Modifier) {
+
     var lucky = (1..100).random()
     Text(
         text = "樂透數字(1-100)為 $lucky",
         modifier = modifier
     )
+    Button(
+        onClick = { lucky = (1..100).random() }
+    ) {
+        Text("重新產生樂透碼")
+    }
 }
